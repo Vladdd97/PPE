e4005a4b9a0e5a525c93481ca9f06704db12d091
Vladdd97 <vladbantus@mail.ru>
2018-04-13 00:03:17 +0300

    Lab#4

---
+++ Lab#4/PPE[ Lab-4 ]/PPE[ Lab-4 ]/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPE__Lab_4__
+{
+
+    public enum Collision
+    {
+        Left, Right, Up, Down , None
+    }
+
+    public partial class Form1 : Form
+    {
+
+
+
+        private Graphics graphics;
+        private SolidBrush brush;
+        private List<Object> objects;
+        private int dimensionX;
+        private int dimensionY;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            DoubleBuffered = true; // need to solve flickering problem
+            objects = new List<Object>();
+            this.MouseWheel += from_MouseWheel; // need for creating MouseWheel event
+            dimensionX = 40;
+            dimensionY = 40;
+
+
+        }
+
+
+        private void from_MouseWheel(object sender, MouseEventArgs e)
+        {
+            this.label2.Text = "Speed : " + (101-this.timer1.Interval);
+
+            if (e.Delta > 0) // need to check MouseWheel rotation (+up) (-down)
+            {
+                if (this.timer1.Interval > 1)
+                {
+                    this.timer1.Interval--;
+                }
+                else
+                {
+                    MessageBox.Show("It is the max speed");
+                }
+            }
+
+            if (e.Delta < 0)
+            {
+                if (this.timer1.Interval < 100)
+                {
+                    this.timer1.Interval++;
+                }
+                else
+                {
+                    MessageBox.Show("It is the min speed");
+                }
+            }
+
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        { 
+            foreach (var obj in objects)
+            {
+               e.Graphics.FillEllipse(obj.brush,obj.PosX,obj.PosY,obj.DimensionX,obj.DimensionY);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var obj in objects)
+            {
+                obj.MoveObj(); // change object PosX and PosY
+                Collision(obj); // check collision
+            }
+            Invalidate(); // clear form
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int speedX = 2, speedY = 2;
+            bool create = true;
+            foreach (var obj in objects)
+            {
+                if (Math.Abs(e.X - obj.PosX) <= obj.DimensionX && Math.Abs(e.Y - obj.PosY) <= obj.DimensionY)
+                {
+                    create = false;
+                    MessageBox.Show("You can not create a ball inside another ball !");
+                }
+
+            }
+
+
+            if (e.X + dimensionX >= this.ClientSize.Width || e.Y + dimensionY >= this.ClientSize.Height)
+            {
+                create = false;
+                MessageBox.Show("You can not create a ball out of Form's Area !");
+            }
+
+            double areaOfObject = 2 * Math.PI * Math.Pow((dimensionX / 2), 2);
+            double areaOfForm = this.ClientSize.Width * this.ClientSize.Height;
+            if (objects.Count * areaOfObject > areaOfForm/6 )
+            {
+                create = false;
+                MessageBox.Show("The area of Form is too small !!!" +
+                                "\nIf you want to add more objects please resize the Form . " +
+                                "\n( Make it bigger) !!!");
+
+
+            }
+
+            if (create)
+            {
+                if (Object.count % 4 == 3)
+                {
+                    speedX = -2;
+                }
+                if (Object.count % 4 == 2)
+                {
+                    speedY = -2;
+                }
+                if (Object.count % 4 == 1)
+                {
+                    speedX = -2;
+                    speedY = -2;
+                }
+
+                objects.Add(new Object(e.X, e.Y, speedX, speedY, dimensionX, dimensionY));
+            }
+
+
+        }
+
+
+        public void Collision(Object obj)
+        {
+            bool collision = false; // need to avoid bags on bounds
+
+            if (obj.PosX + obj.DimensionX >= this.ClientSize.Width)
+            {
+                collision = true;
+                obj.MoveX = -obj.MoveX;
+                obj.ChangeColor();
+            }
+            if (obj.PosX <= 0)
+            {
+                collision = true;
+                obj.MoveX = -obj.MoveX;
+                obj.ChangeColor();
+            }
+
+            if (obj.PosY + obj.DimensionY >= this.ClientSize.Height)
+            {
+                collision = true;
+                obj.MoveY = -obj.MoveY;
+                obj.ChangeColor();
+            }
+            if (obj.PosY <= 0)
+            {
+                collision = true;
+                obj.MoveY = -obj.MoveY;
+                obj.ChangeColor();
+            }
+
+            if (collision)
+            {
+                return ; // if there is a collision on form's bounds then don't check other collisions
+            }
+
+            foreach (var obj2 in objects)
+            {
+                if (obj.Id == obj2.Id)
+                    continue;
+
+                if (Math.Sqrt(Math.Pow((obj.CenterX - obj2.CenterX), 2) + Math.Pow((obj.CenterY - obj2.CenterY), 2)) < obj.DimensionX)
+                {
+                    if (obj.CenterX < obj2.CenterX || obj.CenterX > obj2.CenterX)
+                    {
+                        obj.MoveX = -obj.MoveX;
+                    }
+
+                    if (obj.CenterY < obj2.CenterY || obj.CenterY > obj2.CenterY)
+                    {
+                        obj.MoveY = -obj.MoveY;
+                    }
+
+                    obj.ChangeColor();
+
+                    break;
+                }
+
+            }
+
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.B)
+            {
+                if (this.timer1.Enabled == true)
+                {
+                    this.timer1.Enabled = false;
+                }
+                else
+                {
+                    this.timer1.Enabled = true;
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Lab#4/PPE[ Lab-4 ]/PPE[ Lab-4 ]/Object.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE__Lab_4__
+{
+
+    public class Object
+    {
+        public static int count = 0;
+
+        public float CenterX { get; set; }
+        public float CenterY { get; set; }
+
+        public int Id { get; set; }
+        public float DimensionX { get; set; }
+        public float DimensionY { get; set; }
+
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+
+        public float MoveX { get; set; }
+        public float MoveY { get; set; }
+
+        public SolidBrush brush { get; set; }
+
+        public Object(float posX , float posY, float moveX, float moveY, float dimensionX, float dimensionY)
+        {
+            count++;
+            Id = count;
+            PosX = posX;
+            PosY = posY;
+
+            MoveX = moveX;
+            MoveY = moveY;
+
+            DimensionX = dimensionX;
+            DimensionY = dimensionY;
+
+            CenterX = PosX + DimensionX / 2;
+            CenterY = PosY + DimensionY / 2;
+            brush = new SolidBrush(Color.Blue);
+        }
+
+        public void MoveObj()
+        {
+            PosX += MoveX;
+            PosY += MoveY;
+
+            CenterX = PosX + DimensionX / 2;
+            CenterY = PosY + DimensionY / 2;
+        }
+
+
+        public void ChangeColor()
+        {
+            this.brush.Color = this.brush.Color == Color.Blue ? this.brush.Color = Color.Red : this.brush.Color = Color.Blue;
+        }
+
+
+
+    }
+
+}
